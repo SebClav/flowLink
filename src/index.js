832c243ba6348491d5896c3c12fe46c4bab4ae23
SebClav <sebasclavijoalessandri@gmail.com>
2025-01-02 09:54:18 -0400

    Add imports for styles and theme in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import flowLinkTheme from './style.js';
+
 import './index.css';
+import {CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={flowLinkTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
