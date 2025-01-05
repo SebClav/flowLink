248a5478145ca9494fa278bd29ecd3f2b99ebc34
SebClav <sebasclavijoalessandri@gmail.com>
2025-01-05 00:41:40 -0400

    Add react-router routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import {Container} from '@mui/material'
 import './App.css';
 import Navbar from './components/navbar/navbar.js';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home.js';
+import Client from './pages/client.js';
+import Truck from './pages/truck.js';
+import Product from './pages/product.js';
+import DeliveryRoutes from './pages/routes.js';
+import Order from './pages/order.js';
 
 function App() {
   return (
-    <Container sx={{
-      mt: 5
-      }}>
-
+    <>
       <Navbar />
-    </Container>
+      <Container sx={{
+        mt: 5
+        }}>
+        <Routes>
+          <Route path='/' element={ <Home/>}/>
+          <Route path='/product' element={ <Product/>}/>
+          <Route path='/client' element={ <Client/>}/>
+          <Route path='/route' element={ <DeliveryRoutes/>}/>
+          <Route path='/truck' element={ <Truck/>}/>
+          <Route path='/order' element={ <Order/>}/>
+        </Routes>
+      </Container>
+    </>
   );
 }
 
